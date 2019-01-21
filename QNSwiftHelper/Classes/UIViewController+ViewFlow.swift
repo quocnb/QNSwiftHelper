929d376af31ef28321ed105356e1324d4366338d
Quoc Nguyen <quocnb.vnu@gmail.com>
2019-01-21 10:45:48 +0900

    Add load viewcontroller from xib

--- QNSwiftHelper/Classes/UIViewController+ViewFlow.swift
+++ QNSwiftHelper/Classes/UIViewController+ViewFlow.swift
@@ -8,16 +8,6 @@
 import UIKit
 
 public extension UIViewController {
-    static var identifier: String {
-        return String(describing: self)
-    }
-
-    static func controller<T: UIViewController>(type: T.Type, at storyboardName: String = "Main") -> T {
-        let storyboard = UIStoryboard(name: storyboardName, bundle: nil)
-        let identifier = T.identifier
-        return storyboard.instantiateViewController(withIdentifier: identifier) as? T ?? T()
-    }
-
     func push<T: UIViewController>(_ type: T.Type, from storyboardName: String = "Main", configure: ((T) -> Void)?) {
         let vc = UIViewController.controller(type: type, at: storyboardName)
         configure?(vc)
