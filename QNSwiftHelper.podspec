bccb3bbb3f54c3abb82e782e2ac565de2f2d2893
Quoc Nguyen <quocnb.vnu@gmail.com>
2019-01-24 17:31:00 +0900

    Auto detect view with same storyboard when push or present

--- QNSwiftHelper.podspec
+++ QNSwiftHelper.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'QNSwiftHelper'
-  s.version          = '0.5.0'
+  s.version          = '0.6.0'
   s.summary          = 'Quick Helper for iOS project'
 
 # This description is used to generate tags and improve search results.
--- QNSwiftHelper/Classes/UIViewController+ViewFlow.swift
+++ QNSwiftHelper/Classes/UIViewController+ViewFlow.swift
@@ -8,14 +8,24 @@
 import UIKit
 
 public extension UIViewController {
-    func push<T: UIViewController>(_ type: T.Type, from storyboardName: String = "Main", configure: ((T) -> Void)?) {
-        let vc = UIViewController.controller(type: type, at: storyboardName)
+    func viewController<T: UIViewController>(type: T.Type, from storyboardName: String? = nil) -> T {
+        if let storyboardName = storyboardName {
+            return UIViewController.controller(type: type, at: storyboardName)
+        } else if let vc = self.storyboard?.instantiateViewController(withIdentifier: T.identifier) as? T {
+            return vc
+        } else {
+            return T()
+        }
+    }
+
+    func push<T: UIViewController>(_ type: T.Type, from storyboardName: String? = nil, configure: ((T) -> Void)?) {
+        let vc = viewController(type: type, from: storyboardName)
         configure?(vc)
         self.navigationController?.pushViewController(vc, animated: true)
     }
 
-    func present<T: UIViewController>(_ type: T.Type, _ hasNavigation: Bool, configure: ((T) -> Void)?) {
-        let vc = UIViewController.controller(type: type)
+    func present<T: UIViewController>(_ type: T.Type, _ hasNavigation: Bool, from storyboardName: String? = nil, configure: ((T) -> Void)?) {
+        let vc = viewController(type: type, from: storyboardName)
         let presentVC: UIViewController!
         if hasNavigation {
             let nav = UINavigationController(rootViewController: vc)
