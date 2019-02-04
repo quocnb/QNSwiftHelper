a46f19b2e9e087c8fa75449f84ab1f3e55358ee6
Quoc Nguyen <quocnb.vnu@gmail.com>
2019-02-04 18:26:30 +0900

    [Version 0.7.0] [TableView, CollectionView] Safe reload, register and reuse header, footer

--- QNSwiftHelper/Classes/UITableView+Extension.swift
+++ QNSwiftHelper/Classes/UITableView+Extension.swift
@@ -7,6 +7,16 @@
 
 import UIKit
 
+// MARK: - SAFE LOADING
+public extension UITableView {
+    func safeReloadData() {
+        DispatchQueue.main.async {
+            self.reloadData()
+        }
+    }
+}
+
+// MARK: - REGISTER AND REUSE
 public extension UITableView {
     func register<T: UITableViewCell>(_ cellType: T.Type, fromNib: Bool = true) {
         if fromNib {
@@ -16,10 +26,24 @@ public extension UITableView {
         }
     }
 
+    func registerHeaderFooter<T: UITableViewHeaderFooterView>(_ type: T.Type, fromNib: Bool = true) {
+        if fromNib {
+            self.register(T.nib, forHeaderFooterViewReuseIdentifier: T.identifier)
+        } else {
+            self.register(T.self, forHeaderFooterViewReuseIdentifier: T.identifier)
+        }
+    }
+
     func reuseCell<T: UITableViewCell>(_ cellType: T.Type, for indexPath: IndexPath) -> T {
         let reuse = self.dequeueReusableCell(withIdentifier: cellType.identifier, for: indexPath)
         return reuse as? T ?? T()
     }
+
+    func reuseHeaderFooter<T: UITableViewHeaderFooterView>(_ type: T.Type, for indexPath: IndexPath) -> T {
+        let reuse = self.dequeueReusableHeaderFooterView(withIdentifier: T.identifier)
+        return reuse as? T ?? T()
+    }
+
 }
 
 extension UITableViewCell {
